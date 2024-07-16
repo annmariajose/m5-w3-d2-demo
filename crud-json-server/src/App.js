c0cffa0be6676d14a2b02807176c3223d10baf41
Ann Maria Jose <annmariajose1412@gmail.com>
2024-07-16 18:29:47 -0500

    Get list of data from json server file

--- crud-json-server/src/App.js
+++ crud-json-server/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Lists from "./Lists";
 
 class App extends React.Component {
     constructor(props) {
@@ -11,6 +12,40 @@ class App extends React.Component {
           author: ""
         }
       };
+    }
+
+    getLists = () => {
+      fetch("http://localhost:5000/posts")
+      .then(res => res.json())
+      .then(result => 
+        this.setState({
+          loading: false,
+          alldata: result
+        })
+      )
+      .catch(console.log);
+    }
+    
+    render() {
+      const listTable = this.state.loading ? (
+        <span>Loading Data......Please be patient.</span>
+      ) : (
+        <Lists alldata={this.state.alldata} />
+      );
+      return(
+        <div className="container">
+          <span className="title-bar">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.getLists}
+            >
+              Get Lists
+            </button>
+          </span>
+          {listTable}
+        </div>
+      )
     }
   }
 
